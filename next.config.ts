116ca3660b8a674fc2ed1d45cb63f1a9acaf5950
kuslhhh <kushaljadhav7745@gmail.com>
2024-11-20 23:26:40 +0530

    shadcn added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imagedump.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "c.saavncdn.com",
+      },
+    ],
+  },
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    BACKEND_URI: process.env.BACKEND_URI,
+    MONGODB_URL: process.env.MONGODB_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    SOCKET_URI: process.env.SOCKET_URI,
+  },
 };
 
 export default nextConfig;
